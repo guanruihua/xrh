b98aa004763bb090db5a564f65a2a647b6f7e740
grh-gitee <3494242908@qq.com>
2022-03-30 17:14:19 +0800

    save

--- xrh.js
+++ xrh.js
@@ -1,4 +1,10 @@
 const args = process.argv
+let glob = undefined
+try {
+  glob = require("glob")
+} catch (_err) {
+  // treat glob as optional.
+}
 const log = help ? console.log : console.error
 function xrh(){
 	log('log, log, log')
